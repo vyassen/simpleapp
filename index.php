4e15f8dc770bc0ef5f9ce05804ca1202faf3d205
vyassen <abdelrahman.yassen@hotmail.com>
2022-08-17 02:26:00 +0400

    update index.html

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 		<?php echo "The Current Date and Time is: <br />"; 
 		echo date("g:i A l, F j Y.");?> </p>
 	<h2>PHP Information</h2> 
-		<p> <?php phpinfo(); ?> </p> 
         <h2>GOING ON VACATION</h2>
 	</body> 
 </html>
